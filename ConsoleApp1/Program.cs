bcc0ab392e14feb90cafef8231402f45c725b45c
Haqe Salo <haqe.s2@gso.schule.koeln>
2022-01-26 14:07:25 +0100

    Projektdateien hinzufügen.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Console.WriteLine("Bitte geben Sie ein Zahl ein");
+            i = Convert.ToInt32(Console.ReadLine());
+            //double e = i * 2;
+            //Console.WriteLine(i - e);
+            //double b = i - e;
+
+            //while (b >= i)
+            //{
+            //    Console.WriteLine(b++);
+            //}
+            for(int a =i; a>=-i;a--)
+            {
+                Console.WriteLine(a);
+            }
+        }
+   }
+}   
