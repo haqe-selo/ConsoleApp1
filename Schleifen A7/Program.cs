78c09bac3aeed00b6628a83ae8c41d0e58b1eacb
Haqe Salo <haqe.s2@gso.schule.koeln>
2022-01-27 10:40:56 +0100

    Schleife & fertig

---
+++ Schleifen A7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Schleifen_A7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Console.WriteLine("Bitte geben Sie ein Zahl ein");
+            i = Convert.ToInt32(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine(i--);
+                Console.WriteLine();
+            }
+            while (i >= 0);
+        }
+    }
+}
---
+++ SchleifenA6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SchleifenA6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            int a;
+            
+
+            do
+            {
+                Console.WriteLine("Menü");
+                a = Convert.ToInt32(Console.ReadLine());
+            }
+            while (a!=0);
+        }
+    }
+}
