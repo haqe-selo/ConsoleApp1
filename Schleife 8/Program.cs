d3c159c23d2ac9e45ea63fddd17923cfd2a05877
Haqe Salo <haqe.s2@gso.schule.koeln>
2022-01-27 11:39:19 +0100

    Schleife A8 fertig

---
+++ Schleife 8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Schleife_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            Console.WriteLine("Geben Sie bitte den Kennwort ein");
+            x = Convert.ToInt32(Console.ReadLine());
+           
+            while (x != 1234)
+            {
+                Console.WriteLine("Geben Sie bitte den Kennwort ein");
+                x = Convert.ToInt32(Console.ReadLine());
+            }
+            
+        }
+    }
+}
